c3cc415ade8c20c0af053ea2cd053b14f7335750
Train, Penny <Penny.Train@watlow.com>
2025-11-29 22:26:34 +0000

    Add to visuals.py, more analysis button and window preferences
    Added more analysis buttons, and edit the window sizes along with the blurryness of tkinter

--- visuals.py
+++ visuals.py
@@ -1,12 +1,32 @@
 import tkinter as tk
 from core_calculations import avg_grade, avg_attendance, num_passed, num_grades, num_failed
+from tkinter import ttk
+# ttk contains the new widgets from 2007
 
+# create an instance of the tk.Tk class, this will create the application window 
+# convention: the main window in Tkinter is called root
 root = tk.Tk()
-root.title('Learning')
-root.geometry('500x400')
 
-title = tk.Label(root, text='Welcome to my app')
-title.pack(pady=10)
+# CONTROLLING THE WINDOW
+# This changes the title of the window
+root.title('PennysProj')
+# window geomtry/ size and postion of the window
+# width x height and then +x +y 
+# +y = 50, means that the windows veritcal position will be 50 pixels below the top of the screen
+# root.geometry('600x400+50+50')
+window_width = 500
+window_height = 500
+# get the screen dimension
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+# find the center point
+center_x = int(screen_width/2 - window_width / 2)
+center_y = int(screen_height/2 - window_height / 2)
+# set the position of the window to the center of the screen
+root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+# Title of the window
+message = tk.Label(root, text="Student Grade Analysis")
+message.pack()
 
 result_label = tk.Label(root, text="")
 result_label.pack(pady=10)
@@ -21,4 +41,15 @@ tk.Button(root, text="Number Of Passes", command=lambda: show("Number Of Passes"
 tk.Button(root, text="Number Of Fails", command=lambda: show("Number Of Fails", num_failed())).pack(pady=10)
 tk.Button(root, text="Number Of Grades", command=lambda: show("Number Of Grades", num_grades())).pack(pady=10)
 
-root.mainloop()
+try:
+    # this stops the text from being blury
+    from ctypes import windll
+    windll.shcore.SetProcessDpiAwareness(1)
+finally:
+    # mainloop ensures the window remains visible, without it will appear and disappear quickly
+    root.mainloop()
+
+
+#REFERENCES
+#https://www.pythontutorial.net/tkinter/tkinter-ttk/
+
